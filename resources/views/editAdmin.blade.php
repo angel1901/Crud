c3c074dff1c8eb7a09d5aaca88a0d4f0a3034fb4
Angel Ramirez <angel.ramirez@ariadnacg.com>
2022-07-28 14:01:22 -0500

    Ajuste Vista Admin

--- resources/views/editAdmin.blade.php
+++ resources/views/editAdmin.blade.php
@@ -4,17 +4,17 @@
     @foreach ($users as $u)
         <form class="row m-0" method="POST" action="/updateAdmin/{{ $u->id }}" >
             @csrf
-                <div class="col-6 mb-3">
+                <div class="col-lg-6 mb-3">
                     <label for="name" class="form-label m-0">Nombre</label>
                     <input type="" class="form-control" id="name" name="name" value="{{ $u->name }}">
                     {{ $errors->first('name') }}
                 </div>
-                <div class="col-6 mb-3">
+                <div class="col-lg-6 mb-3">
                     <label for="email" class="form-label m-0">Email</label>
                     <input type="" class="form-control" id="email" name="email" value="{{ $u->email }}">
                     {{ $errors->first('email') }}
                 </div>
-            <div class="col-6 my-4 text-center">
+            <div class="col-lg-6 my-4 text-center">
             <button type="submit" class="col-8 mx-auto btn btn-success">Crear</button>
             </div>
         </form>
